e61e3bd829c527cd362675807f25647fdf27a91b
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2022-05-25 09:34:13 -0300

    Tipos de dados

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
+/*  * Data types
+        *pritive
+        *structural
+        *structural primitive
+    
+    * Primitivos
+        *String
+        *Number
+        *Boolean
+        *undefined
+        *Symbol
+        *BigInt
+    
+    * Estruturais
+        *object
+            *Array
+            *Map
+            *Set
+            *Date
+            *...
+            
+        *fubction
+    * Primitivo Estrutural
+        *null
+
+*/
+
 console.log("ola mundo");
+
+//object ex
+console.log({
+    name:"persio",
+    idade:"26",
+    andar: function(){
+        console.log('andar')
+    }
+});
+
+//array ex
+console.log([
+    "leite",
+    "ovo",
+    2,
+    3
+]);
