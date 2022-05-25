165fcd6750387701391eb1b21fb61a3b43c0243e
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2022-05-25 19:50:47 -0300

    ...

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
             *Set
             *Date
             *...
-            
+
         *fubction
     * Primitivo Estrutural
         *null
@@ -43,3 +43,32 @@ console.log([
     2,
     3
 ]);
+
+//variael
+var ex = "variavel, global e podera funcionar fora do escopo de bloco";
+let Ex = "let funciona parecido com o var, local";
+const exemplo = "constante uma vez definido o valor nao pode ser alterado";
+// weakly typed, the type is defined by the assigned value.
+let name = "persio";
+let age, isHuman;
+
+age = 26;
+isHuman = true;
+
+
+//escritade texto + variavel
+//concatenando os valores
+console.log('concatenando: o ' + name + ' tem ' + age + ' anos.');
+
+//interpolando valores com template literals
+console.log(`interpolando: o ${name} tem ${age} anos.`);
+
+//object
+
+const person = {
+    name: 'Persio',
+    age: 26,
+    weight: 70,
+    isAdmin: true
+}
+console.log(person)
